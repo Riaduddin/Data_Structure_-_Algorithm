098f9b6fa9ab24b7d40cdd287762f1112b872b5b
Riaduddin <rrriaduddin@gmail.com>
2022-05-30 19:07:12 +0600

    finding unique_emails & commenting,completing the divide two_integers

--- Array/Divide_two_integers_without_division_operators.py
+++ Array/Divide_two_integers_without_division_operators.py
@@ -1,37 +1,32 @@
-
-# def division(dividend,divisor):
-#     count=0
-#     while dividend>=divisor:
-#         dividend=dividend-divisor
-#         count+=1
-#     return count
-        
+#divide two integers using '<<' shifting operators instead of
+#using any mathematical operators
 def division(dividend,divisor):
+    #checking the queotient sign
+    sign=( -1 if ((dividend<0)^(divisor<0)) else 1)
     count=0
+    dividend=abs(dividend)
+    divisor=abs(divisor)
+    #looping until the dividend value is bigger
     while (dividend-divisor)>=0:
         i=0
+        #checking which number of shifting operation is bigger than dividend
         while(dividend-(divisor<<i))>=0:
             i+=1
+        #decrease the dividend value to reiterate
         dividend=dividend-(divisor<<(i-1))
+        #calculating the number of queotient value
         temp=1<<(i-1)
+        #saving the value
         count+=temp
         if dividend==divisor:
             count+=1
+            #changing the value to zero
+            #not to reiterate one time again
             dividend=dividend-divisor
-    queotinent=count
-    return queotinent
+    queotient=count
+    queotient=sign *queotient
+    return min(2147483647,max(-2147483648,queotient))
     
-
-
-
-    
+ 
         
-print(division(1586,3))
-
-#print(division(57,13))
-# value=0
-# value=1<<5
-#print(1<<2)
-#print(value)
-#print(2<<2)
-#print(value)
+print(division(-2147483648,-1))
