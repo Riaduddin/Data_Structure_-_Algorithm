f3cf3ed79b5767f8d05683f9b0897414510a0f17
Riaduddin <rrriaduddin@gmail.com>
2022-06-13 18:02:16 +0600

    quicksort algorithm implementation

---
+++ Array/quicksort_implementation.py
@@ -0,0 +1,26 @@
+#dividing the array
+def partition(arr,p,q):
+    x=arr[p]
+    i=p
+    for j in range(p+1,q+1):
+        if arr[j]<x:
+            i=i+1
+            temp=arr[i]
+            #swapping the value
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[p],arr[i]=arr[i],arr[p]
+    return i
+#quicksort implementation
+def quicksort(arr,p,q):
+    #single array
+    if p==q:
+        return arr[p]
+    if p<q:
+        mid=partition(arr,p,q)
+        #recursive function
+        quicksort(arr,p,mid-1)
+        quicksort(arr,mid+1,q)
+
+arr=[50,70,80,30,40,88,19,27,69]
+quicksort(arr,0,len(arr)-1)
+print(arr)
